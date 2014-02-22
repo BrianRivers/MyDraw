6972eea3d71a41b6e465ebce3f1668378799cc38
Brian Rivers <brianrivers0015@gmail.com>
2014-02-22 00:48:56 -0600

    Working Draw feature implemented

--- src/com/example/mydraw/DrawPad.java
+++ src/com/example/mydraw/DrawPad.java
@@ -1,11 +1,25 @@
 package com.example.mydraw;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class DrawPad extends View {
-
+	
+	private List<Float> xPosition;
+	private List<Float> yPosition;
+	private Paint pictureLine;
+	private Path path;
+	
 	public DrawPad(Context context) {
         this(context, null, 0);
     }
@@ -16,6 +30,56 @@ public class DrawPad extends View {
 
     public DrawPad(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        
+        pictureLine = new Paint();
+		pictureLine.setColor(Color.WHITE);
+		pictureLine.setStyle(Paint.Style.STROKE);
+		
+		xPosition = new ArrayList<Float>();
+		yPosition = new ArrayList<Float>();
+		
+		path = new Path();
+    }
+
+    @Override
+	public boolean onTouchEvent(MotionEvent event){
+    	
+    	//xPosition.add(event.getX());
+    	//yPosition.add(event.getY());
+    	
+    	float tempX = event.getX();
+    	float tempY = event.getY();
+    	
+		switch (event.getAction()) {
+	    case MotionEvent.ACTION_DOWN:
+	        Log.i("Test Event", "<------------- DOWN ------------- >");
+	        path.moveTo(tempX, tempY);
+	        break;
+	    case MotionEvent.ACTION_MOVE:
+	    	Log.i("Test Event", "<------------- MOVE ------------- >");
+	    	//Log.i("Test Event", "<------------- " + xPosition + " , " + yPosition + " ------------- >");
+	    	path.lineTo(tempX, tempY);
+	        break;
+	    case MotionEvent.ACTION_UP:
+	    	Log.i("Test Event", "<------------- UP ------------- >");
+	    	//Log.i("Test Event", "<------------- " + xPosition + " , " + yPosition + " ------------- >");
+	    	///path.reset();
+	        break;
+		}
+		
+		invalidate();
+		return true;
+	}
+    
+    @Override
+    protected void onDraw(Canvas canvas) {
+    	super.onDraw(canvas);
+    	
+    	//for (int count = 0; count < xPosition.size(); count++){
+    		//canvas.drawPoint(xPosition.get(count), yPosition.get(count), pictureLine);
+    		canvas.drawPath(path, pictureLine);
+    	//}
+    	
     }
 
 }
--- src/com/example/mydraw/MainActivity.java
+++ src/com/example/mydraw/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.mydraw;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnTouchListener {
+	
+	DrawPad mainCanvas;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mainCanvas = (DrawPad) findViewById(R.id.mainCanvas);
+		
+		mainCanvas.setOnTouchListener(this);
 	}
 
 	@Override
@@ -17,6 +26,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
